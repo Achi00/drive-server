336c7287b678ae7b23acf4f37fe343bfcb2654ef
Achi00 <toolvlg@gmail.com>
2024-06-04 21:32:04 +0400

    sharp error

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
